a10a7149b71012853f01c643662612191ca00ad7
hamid <imporham@gmail.com>
2021-09-19 17:45:13 +0430

    some code cleaning some configuration at package.json and main.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MailerProviderService } from './providers/mail/provider.service';
-import { TestMailJobProducer } from './jobs/producers/test-mail.job.producer';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly mailerService: MailerProviderService,
-    private readonly testMailJobProducer: TestMailJobProducer,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/mail')
-  mailTest(): void {
-    this.testMailJobProducer.dispatch('hey_you808@yahoo.com');
   }
 }
