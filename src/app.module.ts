ca8e6d7696ebc1a4b6d54b58e0b2660d4e279f96
hamid <imporham@gmail.com>
2021-09-19 18:22:53 +0430

    some code cleaning in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/configuration.module';
-import { MailerProviderModule } from './providers/mail/provider.module';
-import { QueueProviderModule } from './providers/queue/provider.module';
-import { BullModule } from '@nestjs/bull';
-import { TestMailJobProducer } from './jobs/producers/test-mail.job.producer';
-import { MailJobConsumer } from './jobs/consumers/mail.job.consumer';
 import { LoggerProviderModule } from './providers/logger/provider.module';
 import { MongooseProviderModule } from './providers/database/mongoose/provider.module';
 
@@ -15,13 +10,8 @@ import { MongooseProviderModule } from './providers/database/mongoose/provider.m
     AppConfigModule,
     LoggerProviderModule,
     MongooseProviderModule,
-    QueueProviderModule,
-    MailerProviderModule,
-    BullModule.registerQueue({
-      name: 'mail',
-    }),
   ],
   controllers: [AppController],
-  providers: [AppService, TestMailJobProducer, MailJobConsumer],
+  providers: [AppService],
 })
 export class AppModule {}
